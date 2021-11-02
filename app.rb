2121338456888965b12ca1eceb2cb44036ed76d0
Strazdin_Dmitry <dmitrijstrazdin5@gmail.com>
2021-11-02 17:51:12 +0300

    Add /details/...handler

--- app.rb
+++ app.rb
@@ -70,3 +70,11 @@ post '/new' do
 
 	redirect to '/'
 end
+
+# вывод информации о посте (универсальный обработчик)
+
+get '/details/:post_id' do
+	post_id = params[:post_id]
+
+	erb "Displaying information for post with id #{post_id}"
+end
