ded353d6fbdcc2be48429505c40f986ccb705eca
Strazdin_Dmitry <dmitrijstrazdin5@gmail.com>
2021-11-02 17:15:55 +0300

    Add index.erb

--- app.rb
+++ app.rb
@@ -36,7 +36,7 @@ configure do
 end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index
 end
 
 	# обработчик get-запроса /new
@@ -54,12 +54,14 @@ post '/new' do
 	# получаем переменную из post-запроса
 	content = params[:content]
 
-		if content.length <= 0
-			@error = 'Type post text'
-			return erb :new
-		end
+	if content.length <= 0
+		@error = 'Type post text'
+		return erb :new
+	end
 
-		@db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
+	# сохранение данных в БД
+
+	@db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
 
 	erb "You taped #{content}"
 end
